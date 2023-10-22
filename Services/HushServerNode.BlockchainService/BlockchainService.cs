8aece51274df502d46d185935a2c8237f4ba1773
Paulo Aboim Pinto <aboimpinto@gmail.com>
2023-10-22 22:45:34 +0000

    Add BlockchainService

--- Services/HushServerNode.BlockchainService/BlockchainService.cs
+++ Services/HushServerNode.BlockchainService/BlockchainService.cs
@@ -1,9 +1,19 @@
-﻿namespace HushServerNode.BlockchainService;
+﻿using Microsoft.Extensions.Logging;
+
+namespace HushServerNode.BlockchainService;
 
 public class BlockchainService : IBlockchainService
 {
+    private readonly ILogger<BlockchainService> _logger;
+
+    public BlockchainService(ILogger<BlockchainService> logger)
+    {
+        this._logger = logger;
+    }
+
     public Task InitializeBlockchainAsync()
     {
+        this._logger.LogInformation("Initializing Blockchain...");
         return Task.CompletedTask;
     }
 }
